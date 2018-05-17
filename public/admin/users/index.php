1d3a93d61e64f3aac2d664504cf8a450453d0b39
Abarri <officiel.abarri@gmail.com>
2018-05-17 14:47:35 +0100

    minor change

--- public/admin/users/index.php
+++ public/admin/users/index.php
@@ -1,10 +1,19 @@
-<?php include "templates/header.php"; ?>
+<?php include "../../templates/header.php"; ?>
 
-<ul>
-	<li><a href="create.php"><strong>Create</strong></a> - add a user</li>
-	<li><a href="read.php"><strong>Read</strong></a> - find a user</li>
-	<li><a href="update.php"><strong>Update</strong></a> - edit a user</li>
-	<li><a href="delete.php"><strong>Delete</strong></a> - delete a user</li>
-</ul>
-
-<?php include "templates/footer.php"; ?>
+<?php include "../../shared/navbar.php"; ?>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="card card-body bg-light mt-5">
+          <h2>Administration d'utilisateurs</h2>
+					<ul>
+						<li><a href="create.php"><strong>Create</strong></a> - add a user</li>
+						<li><a href="read.php"><strong>Read</strong></a> - find a user</li>
+						<li><a href="update.php"><strong>Update</strong></a> - edit a user</li>
+						<li><a href="delete.php"><strong>Delete</strong></a> - delete a user</li>
+					</ul>
+        </div>
+      </div>
+    </div>
+  </div>
+<?php include "../../templates/footer.php"; ?>
--- public/shared/navbar.php
+++ public/shared/navbar.php
@@ -22,7 +22,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#"><img src="http://placehold.it/150x50&text=Logo"></a>
+				<a class="navbar-brand" href="/evaluation_ISGA/public/admin"><img src="http://placehold.it/150x50&text=Logo"></a>
 			</div>
 			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -30,26 +30,26 @@
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown"> Utilisateurs</a>
 						<ul class="dropdown-menu">
-							<li><a href="#">Gestion des utilisateurs</a></li>
-							<li><a href="#">Gestion des types d'utilisateurs</a></li>
+							<li><a href="/evaluation_ISGA/public/admin/users/index.php">Gestion des utilisateurs</a></li>
+							<li><a href="/evaluation_ISGA/public/admin/usertypes/index.php">Gestion des types d'utilisateurs</a></li>
 						</ul>
 					</li>
   					<li class="dropdown">
   						<a href="#" class="dropdown-toggle" data-toggle="dropdown"> Administration</a>
   						<ul class="dropdown-menu">
-  							<li><a href="#">Gestion des niveaux</a></li>
-  							<li><a href="#">Gestion des filliéres</a></li>
-  							<li><a href="#">Gestion des modules</a></li>
+  							<li><a href="/evaluation_ISGA/public/admin/niveux/index.php">Gestion des niveaux</a></li>
+  							<li><a href="/evaluation_ISGA/public/admin/filieres/index.php">Gestion des filliéres</a></li>
+  							<li><a href="/evaluation_ISGA/public/admin/modules/index.php">Gestion des modules</a></li>
   						</ul>
   					</li>
     					<li class="dropdown">
     						<a href="#" class="dropdown-toggle" data-toggle="dropdown"> Evaluation</a>
     						<ul class="dropdown-menu">
-    							<li><a href="#">Gestion des Questions</a></li>
-    							<li><a href="#">Gestion des types de Questions</a></li>
+    							<li><a href="/evaluation_ISGA/public/admin/question/index.php">Gestion des Questions</a></li>
+    							<li><a href="/evaluation_ISGA/public/admin/questiontypes/index.php">Gestion des types de Questions</a></li>
                   <li class="divider"></li>
-                  <li><a href="#">Gestion des Examens</a></li>
-    							<li><a href="#">Gestion des types de Examens</a></li>
+                  <li><a href="/evaluation_ISGA/public/admin/examens/index.php">Gestion des Examens</a></li>
+    							<li><a href="/evaluation_ISGA/public/admin/examentypes/index.php">Gestion des types de Examens</a></li>
     						</ul>
     					</li>
           <li><a href="/evaluation_ISGA/public/authentication/logout.php">Logout</a></li>
